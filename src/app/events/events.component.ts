f96942ff78d4e074607ad8ede1fe071912029ac3
morgangrace11 <morgangrace720@gmail.com>
2019-06-10 15:36:52 -0400

    added basic format of events

--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { events } from '../events'
 
 @Component({
   selector: 'app-events',
   templateUrl: './events.component.html',
   styleUrls: ['./events.component.scss']
 })
-export class EventsComponent implements OnInit {
 
-  constructor() { }
+export class EventsComponent implements OnInit {
+  events = events;
+  constructor() {
+  }
 
   ngOnInit() {
   }
