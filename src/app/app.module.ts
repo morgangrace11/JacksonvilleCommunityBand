f2a02855fca5084be143910eba399fd86a4051d5
morgangrace11 <morgangrace720@gmail.com>
2019-06-10 14:58:03 -0400

    generated components for project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactComponent } from './contact/contact.component';
+import { EventsComponent } from './events/events.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot([
+      { path: '', component: HomePageComponent }
+    ])
+  ],
+  declarations: [
+    AppComponent,
+    HomePageComponent,
+    NavBarComponent,
+    AboutUsComponent,
+    ContactComponent,
+    EventsComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
