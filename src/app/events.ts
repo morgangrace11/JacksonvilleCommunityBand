f42e452fd6a85ef4b7910ae88cdbf3fceea953b5
morgangrace11 <morgangrace720@gmail.com>
2019-06-10 15:37:03 -0400

    added fake data for the events

---
+++ src/app/events.ts
@@ -0,0 +1,23 @@
+export const events = [
+  {
+    name: 'concert for puppies',
+    date: '2/20/2089',
+    description: 'were doing a concert for pups come join us',
+    where: '1111 atlas ave, jacksonville fl, 32277',
+    time: '2:00 PM'
+  },
+  {
+    name: 'concert for cats',
+    date: '2/20/2089',
+    description: 'were doing a concert for kitties come join us',
+    where: '2222 kj ave, jacksonville fl, 32277',
+    time: '2:00 PM'
+  },
+  {
+    name: 'concert for snakes',
+    date: '2/20/2089',
+    description: 'were doing a concert for sneks come join us',
+    where: '3333 snakey ave, jacksonville fl, 32277',
+    time: '3:00 PM'
+  }
+];
