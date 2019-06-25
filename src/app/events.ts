189d0c9f3959f6091b94778d1feed32117f0cf63
Morgan Grace <morgangrace720@gmail.com>
2019-06-24 20:42:16 -0400

    completed styling for all pages

--- src/app/events.ts
+++ src/app/events.ts
@@ -1,23 +1,8 @@
 export const events = [
   {
-    name: 'concert for puppies',
-    date: '2/20/2089',
-    description: 'were doing a concert for pups come join us',
-    where: '1111 atlas ave, jacksonville fl, 32277',
-    time: '2:00 PM'
-  },
-  {
-    name: 'concert for cats',
-    date: '2/20/2089',
-    description: 'were doing a concert for kitties come join us',
-    where: '2222 kj ave, jacksonville fl, 32277',
-    time: '2:00 PM'
-  },
-  {
-    name: 'concert for snakes',
-    date: '2/20/2089',
-    description: 'were doing a concert for sneks come join us',
-    where: '3333 snakey ave, jacksonville fl, 32277',
-    time: '3:00 PM'
+    name: '4th of July Concert',
+    date: '7/6/2019',
+    where: 'Terry Parker Baptist 7024 Merrill Rd, Jacksonville, FL 32277',
+    time: '11:00 AM'
   }
 ];
